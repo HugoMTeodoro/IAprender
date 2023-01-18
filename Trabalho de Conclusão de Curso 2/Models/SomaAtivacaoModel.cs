e480297cf974019f888e6b478d93504781cb6c72
HugoMTeodoro <hugomteodoro1@gmail.com>
2023-01-18 02:55:31 -0300

    somas e ativacoes IArley

---
+++ Trabalho de Conclusão de Curso 2/Models/SomaAtivacaoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trabalho_de_Conclusão_de_Curso_2.Models
+{
+    public class SomaAtivacaoModel
+    {
+        public List<Result> somasEAtivacoes { get; set; }
+
+        public int IndexList { get; set; }
+    }
+    public class Result
+    {
+        public double soma { get; set; }
+        public double ativacao { get; set; }
+        public string cor { get; set; }
+    }
+    }
