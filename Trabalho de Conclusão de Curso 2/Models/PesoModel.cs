6d7774d656a17e500fcd4913e25ec359cf3038b3
HugoMTeodoro <hugomteodoro1@gmail.com>
2023-01-22 00:37:45 -0300

    ultimo momento antes da backpropagation

---
+++ Trabalho de Conclusão de Curso 2/Models/PesoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trabalho_de_Conclusão_de_Curso_2.Models
+{
+    public class PesoModel
+    {
+        public string Cor { get; set; }
+        public double Peso { get; set; }
+        public int Index { get; set; }
+    }
+}
