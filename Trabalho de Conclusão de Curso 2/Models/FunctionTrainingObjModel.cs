2252bc31c717e80919b960ff85e67ecc3d945867
HugoMTeodoro <hugomteodoro1@gmail.com>
2023-01-26 01:19:33 -0300

    sigmoid falhou.

--- Trabalho de Conclusão de Curso 2/Models/FunctionTrainingObjModel.cs
+++ Trabalho de Conclusão de Curso 2/Models/FunctionTrainingObjModel.cs
@@ -7,28 +7,31 @@ namespace Trabalho_de_Conclusão_de_Curso_2.Models
 {
     public class FunctionTrainingObjModel
     {
+
         public List<PesoModel> pesos { get; set; }
-        public List<SomaAtivacaoModel> somaAtivacoes { get; set; }
-        public List<SomaAtivacaoModel> somaAtivacoesSaida { get; set; }
-        public List<DerivadaModel> derivadasSaida { get; set; }
-        public List<DeltaModel> deltaSaida { get; set; }
-        public double erromedioSoma { get; set; }
-        public List<ErroMedioModel> erroMedioClasse { get; set; }
-        public List<DerivadaModel> derivadasNeuronios { get; set; }
-        public List<DeltaModel> deltaEscondida { get; set; }
-        public double entrVsDeltaAzul { get; set; }
-        public double entrVsDeltaVermelho { get; set; }
-        public double entrVsDeltaVerde { get; set; }
-        public double Momento { get; set; }
-        public double txAprendizagem { get; set; }
-        public double entrVsDeltaEscondidaAzulSobrancelha { get; set; }
-        public double entrVsDeltaEscondidaAzulOlhos { get; set; }
-        public double entrVsDeltaEscondidaAzulBoca { get; set; }
-        public double entrVsDeltaEscondidaVermelhoSobrancelha { get; set; }
-        public double entrVsDeltaEscondidaVermelhoOlhos { get; set; }
-        public double entrVsDeltaEscondidaVermelhoBoca { get; set; }
-        public double entrVsDeltaEscondidaVerdeSobrancelha { get; set; }
-        public double entrVsDeltaEscondidaVerdeOlhos { get; set; }
-        public double entrVsDeltaEscondidaVerdeBoca { get; set; }
+        public double erroMedio { get; set; }
+        /* public List<PesoModel> pesos { get; set; }
+         public List<SomaAtivacaoModel> somaAtivacoes { get; set; }
+         public List<SomaAtivacaoModel> somaAtivacoesSaida { get; set; }
+         public List<DerivadaModel> derivadasSaida { get; set; }
+         public List<DeltaModel> deltaSaida { get; set; }
+         public double erromedioSoma { get; set; }
+         public List<ErroMedioModel> erroMedioClasse { get; set; }
+         public List<DerivadaModel> derivadasNeuronios { get; set; }
+         public List<DeltaModel> deltaEscondida { get; set; }
+         public double entrVsDeltaAzul { get; set; }
+         public double entrVsDeltaVermelho { get; set; }
+         public double entrVsDeltaVerde { get; set; }
+         public double Momento { get; set; }
+         public double txAprendizagem { get; set; }
+         public double entrVsDeltaEscondidaAzulSobrancelha { get; set; }
+         public double entrVsDeltaEscondidaAzulOlhos { get; set; }
+         public double entrVsDeltaEscondidaAzulBoca { get; set; }
+         public double entrVsDeltaEscondidaVermelhoSobrancelha { get; set; }
+         public double entrVsDeltaEscondidaVermelhoOlhos { get; set; }
+         public double entrVsDeltaEscondidaVermelhoBoca { get; set; }
+         public double entrVsDeltaEscondidaVerdeSobrancelha { get; set; }
+         public double entrVsDeltaEscondidaVerdeOlhos { get; set; }
+         public double entrVsDeltaEscondidaVerdeBoca { get; set; }*/
     }
 }
